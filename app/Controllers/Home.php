f467f6bae495325824bf2c7808f28d92faf17383
Janna <jannashireen@gmail.com>
2023-11-24 07:11:53 +0530

    work with models

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,61 +1,88 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\UserModel;
 
 class Home extends BaseController
 {
-    private $db;
-    private $builder;
+    // private $db;
+    // private $builder;
     public function index(): string
     {
         return view('welcome_message');
     }
 
-    public function __construct(){
-        $this->db = db_connect();
-        $this->builder = $this->db->table("users");
-    }
+    // public function __construct(){
+    //     $this->db = db_connect();
+    //     $this->builder = $this->db->table("users");
+    
    public function insertData(){
-    $data = [ 
-[
-    
-    "name"=> "User 2",
-    "email"=> "user2@gmail.com",
-    "phone_no"=> "3465687244"
-],
-[
-    
-    "name"=> "User 3",
-    "email"=> "user3@gmail.com",
-    "phone_no"=> "346342247244"
-]
+    $user_obj = new UserModel();
+    $data = [
+"name" => "sahaala",
+"email" => "sahala@gmail.com",
+"phone_no" => "47736734"
     ];
-    $this->builder->insertBatch($data);
+    $user_obj->insert($data);
+//     $data = [ 
+// [
+    
+//     "name"=> "User 2",
+//     "email"=> "user2@gmail.com",
+//     "phone_no"=> "3465687244"
+// ],
+// [
+    
+//     "name"=> "User 3",
+//     "email"=> "user3@gmail.com",
+//     "phone_no"=> "346342247244"
+// ]
+//     ];
+//     $this->builder->insertBatch($data);
    }
 
    public function updateUser(){
-    $user_id = 2;
-    $updated_data=[
-        'name'=> 'janna',
-        'phone_no' => '3426544732'    ];
+    $user_obj = new UserModel();
+    $user_id = 1;
+    $updated_data = [
+        "name" =>"shifa",
+        'phone_no' => "32562576"
+    ];
+    $user_obj->update($user_id,$updated_data);
+    // $user_id = 2;
+    // $updated_data=[
+    //     'name'=> 'janna',
+    //     'phone_no' => '3426544732'    ];
 
-        $this->builder-> update($updated_data,[
+    //     $this->builder-> update($updated_data,[
 
-            "id" => $user_id
-        ]);
+    //         "id" => $user_id
+    //     ]);
    }
 
    public function deleteUser(){
-    $user_id = 3;
-    $this->builder-> delete([
-        "id"=> $user_id
-    ]);
-   }
-   public function selectData(){
-    // $users = $this->builder->get()->getResult();
-    $user = $this->builder->where("id",2)->get()->getRow();
-    echo "<pre>";
-    print_r($user);
-   }
+
+    $user_obj = new UserModel();
+    $user_id = 2;
+    $user_obj->delete($user_id);
+//     $user_id = 3;
+//     $this->builder-> delete([
+//         "id"=> $user_id
+//     ]);
+//    }
+//    public function selectData(){
+//     // $users = $this->builder->get()->getResult();
+//     $user = $this->builder->where("id",2)->get()->getRow();
+//     echo "<pre>";
+//     print_r($user);
+//    }
  
+}
+
+public function selectData(){
+    $user_obj = new UserModel();
+    $users = $user_obj->findAll();
+    echo "<pre>";
+    print_r($users);
+}
 }
